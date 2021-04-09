53e62d4cdad03d0f4b72ffa98f003ce38440c119
Vinoth Kannan <svkn.87@gmail.com>
2021-04-09 11:13:45 +0530

    FEATURE: create default admin account from input with optional password.
    And save the admin user credentials in `config/dev.yml` file.

--- lib/discourse_dev/config.rb
+++ lib/discourse_dev/config.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 require 'rails'
+require 'highline/import'
 
 module DiscourseDev
   class Config
-    attr_reader :config, :default_config
+    attr_reader :config, :default_config, :file_path
 
     def initialize
       default_file_path = File.join(File.expand_path(__dir__), "config.yml")
-      file_path = File.join(Rails.root, "config", "dev.yml")
+      @file_path = File.join(Rails.root, "config", "dev.yml")
       @default_config = YAML.load_file(default_file_path)
 
       if File.exists?(file_path)
@@ -54,21 +55,9 @@ module DiscourseDev
       settings = config["admin"]
 
       if settings.present?
-        email = settings["email"]
-
-        admin = ::User.create!(
-          email: email,
-          username: settings["username"] || UserNameSuggester.suggest(email),
-          password: settings["password"]
-        )
-        admin.grant_admin!
-        if admin.trust_level < 1
-          admin.change_trust_level!(1)
-        end
-        admin.email_tokens.update_all confirmed: true
-        admin.activate
+        create_admin_user_from_settings(settings)
       else
-        Rake::Task['admin:create'].invoke
+        create_admin_user_from_input
       end
     end
 
@@ -84,6 +73,67 @@ module DiscourseDev
     def method_missing(name)
       name = name.to_s
       config[name] || default_config[name]
+    end
+
+    def create_admin_user_from_settings(settings)
+      email = settings["email"]
+
+      admin = ::User.create!(
+        email: email,
+        username: settings["username"] || UserNameSuggester.suggest(email),
+        password: settings["password"]
+      )
+      admin.grant_admin!
+      if admin.trust_level < 1
+        admin.change_trust_level!(1)
+      end
+      admin.email_tokens.update_all confirmed: true
+      admin.activate
+    end
+
+    def create_admin_user_from_input
+      begin
+        email = ask("Email:  ")
+        password = ask("Password (optional, press ENTER to skip):  ")
+        username = UserNameSuggester.suggest(email)
+
+        admin = ::User.new(
+          email: email,
+          username: username
+        )
+
+        if password.present?
+          admin.password = password
+        else
+          puts "Once site is running use https://localhost:9292/user/#{username}/become to access the account in development"
+        end
+
+        admin.name = ask("Full name:  ") if SiteSetting.full_name_required
+        saved = admin.save
+
+        if saved
+          File.open(file_path, 'a') do | file|
+            file.puts("admin:")
+            file.puts("  username: #{admin.username}")
+            file.puts("  email: #{admin.email}")
+            file.puts("  password: #{password}") if password.present?
+          end
+        else
+          say(admin.errors.full_messages.join("\n"))
+        end
+      end while !saved
+
+      admin.active = true
+      admin.save
+
+      admin.grant_admin!
+      if admin.trust_level < 1
+        admin.change_trust_level!(1)
+      end
+      admin.email_tokens.update_all confirmed: true
+      admin.activate
+
+      say("\nAdmin account created successfully with username #{admin.username}")
     end
   end
 end
